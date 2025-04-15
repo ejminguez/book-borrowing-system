04ab993c5ec857160a5ad1409dfbb8bb1086480d
errolJames59 <erroljamesminguez59@gmail.com>
2025-04-15 10:03:28 +0800

    feat: added get books by id

--- backend/routes/books.py
+++ backend/routes/books.py
@@ -17,3 +17,17 @@ def get_books(db: Session = Depends(get_db)):
         return books.data
     except Exception as e:
         raise HTTPException(status_code=500, detail="Error fetching books from Supabase")
+
+@router.get("/books/{book_id}")
+def get_books_by_id(book_id: str, db:Session = Depends(get_db)):
+    """
+    Get a book by its ID.
+    """
+    try:
+        book = supabase.table("books").select("*").eq("book_id", book_id).single().execute()
+        if not book.data:
+            raise HTTPException(status_code=404, detail="Book not found")
+        return book.data
+    except Exception as e:
+        print("Error fetching book: ", e)
+        raise HTTPException(status_code=500, detail="Error fetching book from Supabase")
